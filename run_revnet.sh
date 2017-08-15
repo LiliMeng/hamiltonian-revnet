38a15f952059d11f035cc44f2afda0329fb9442a
Lili <lilimeng1103@gmail.com>
2017-08-15 12:16:48 -0700

    add hamiltonian-revnet

---
+++ run_revnet.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+./run_cifar_train.py --dataset=cifar-10 --model=hamiltonian-38
+./run_cifar_train.py --dataset=cifar-10 --model=hamiltonian-110
+./run_cifar_train.py --dataset=cifar-10 --model=hamiltonian-164
